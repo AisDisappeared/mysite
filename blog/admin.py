fef743d0bc8b82e2a422129a20469da7e17010e1
Ali seyfi <153763977+AisDisappeared@users.noreply.github.com>
2024-01-16 17:21:14 +0330

    django admin options and features
    added some options in class method for showing the model into admin panel
    such as date_hierarchy , empty_value_display , list_display , list_filter , ordering and search_fields .

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from .models import post
 
+
+# we can use decorators here for registration
+# but we don't need to do it for now
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date' 
+    empty_value_display = "-empty-"
+    list_display = ('title', 'published_date','created_date' , 'status')
+    list_filter = ('status',)
+    search_fields = ('title', 'content')
+    ordering = ('-created_date',)
+
+
 # Register your models here.
-admin.site.register(post) 
+
+admin.site.register(post , PostAdmin)
+ 
