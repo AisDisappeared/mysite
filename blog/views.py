71831b28c4dd3fedfd1a63f3f95d25a5956cabc3
Ali seyfi <153763977+AisDisappeared@users.noreply.github.com>
2024-01-18 14:58:01 +0330

    alternative operation
    i changed the posts with my blog model posts and their elements such as title , content and counted_view and etc ...
    by using template tag and jinja for loop

--- blog/views.py
+++ blog/views.py
@@ -3,14 +3,16 @@ from django.shortcuts import render
 from blog.models import post
 
 def blog_view(request):
-    return render(request , 'blog/blog-home.html')                     
+    rows = post.objects.filter(status = 1)
+    context = {'posts' : rows }
+    return render(request , 'blog/blog-home.html' , context)                     
 
 def blog_single(request):
     return render(request , 'blog/blog-single.html')  
 
 def test(request):
-    rows = post.objects.all()
-    context = {'posts' : rows }
+    rows = post.object.all()
+    context = {'posts' : rows}
     return render(request , 'test.html' , context)
 
 
