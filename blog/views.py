ed8ec23d001223bc9e028a0e74db7b904871de8d
Ali seyfi <153763977+AisDisappeared@users.noreply.github.com>
2024-01-21 16:53:49 +0330

    prev and next buttons
    added by context and lt , get methods to setup previous and next post blog buttons

--- blog/views.py
+++ blog/views.py
@@ -11,13 +11,16 @@ def blog_view(request):
     context = {'posts' : posts }
     return render(request , 'blog/blog-home.html' , context)                     
 
-def blog_single(request,pid):
+def blog_single(request,pid,):
     current_time = timezone.now()
     posts = post.objects.filter(published_date__lte=current_time,status=1)
-    p = get_object_or_404(posts,pk=pid)
+    current_post = get_object_or_404(posts,pk=pid)
+    related_posts = post.objects.filter(category__in = current_post.category.all())
     # auto increment for counting views whenever a post is opened
-    p.counted_view += 1
-    p.save()
-    context = {'post':p}
+    current_post.counted_view += 1
+    current_post.save()
+    context = {'post':current_post , 
+               'prev': related_posts.filter(id__lt=current_post.id).order_by('-id').first(), 'next': 
+               related_posts.filter(id__gt=current_post.id).order_by('-id').first()}
     return render(request , 'blog/blog-single.html',context)  
 
