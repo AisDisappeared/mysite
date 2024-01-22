6ceb5835e2402c2dfbe3beb86d944724845ae94b
Ali seyfi <153763977+AisDisappeared@users.noreply.github.com>
2024-01-22 14:04:16 +0330

    ...
    ...

--- blog/views.py
+++ blog/views.py
@@ -14,13 +14,9 @@ def blog_view(request):
 def blog_single(request,pid,):
     current_time = timezone.now()
     posts = post.objects.filter(published_date__lte=current_time,status=1)
-    current_post = get_object_or_404(posts,pk=pid)
-    related_posts = post.objects.filter(category__in = current_post.category.all())
-    # auto increment for counting views whenever a post is opened
-    current_post.counted_view += 1
-    current_post.save()
-    context = {'post':current_post , 
-               'prev': related_posts.filter(id__lt=current_post.id).order_by('-id').first(), 'next': 
-               related_posts.filter(id__gt=current_post.id).order_by('-id').first()}
+    p = get_object_or_404(posts,pk=pid)
+    p.counted_view += 1
+    p.save()
+    context = {'post':p}
     return render(request , 'blog/blog-single.html',context)  
 
