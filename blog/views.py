7798655d2f1d9ea09e295e33a4bc74b132cbdb82
Ali seyfi <153763977+AisDisappeared@users.noreply.github.com>
2024-01-22 18:30:28 +0330

    prev and next buttons
    prev and next buttons made by using post indexes and ids and matched urls of them at blog single template by using jinja template tags , name of context first key element changed to post with upper p and some additional codes deleted

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render,get_object_or_404
-from blog.models import post
+from django.shortcuts import render, get_object_or_404
+from blog.models import post 
 from django.utils import timezone 
 
 
@@ -7,16 +7,31 @@ from django.utils import timezone
 def blog_view(request):
     current_time = timezone.now()
     # using lte method to filter less than equal
-    posts = post.objects.filter(published_date__lte=current_time,status=1)
-    context = {'posts' : posts }
+    posts = post.objects.filter(status = 1, published_date__lte = current_time)
+    context = {'posts' : posts}
     return render(request , 'blog/blog-home.html' , context)                     
 
-def blog_single(request,pid,):
+def blog_single(request, pid):
+    # calculating the current time
     current_time = timezone.now()
-    posts = post.objects.filter(published_date__lte=current_time,status=1)
-    p = get_object_or_404(posts,pk=pid)
-    p.counted_view += 1
-    p.save()
-    context = {'post':p}
-    return render(request , 'blog/blog-single.html',context)  
+    # set query to get all of the posts from the database
+    all_posts = post.objects.filter(status = 1, published_date__lte = current_time)
+    # set the default id for the post the user is requesting to watch that 
+    current_index = 0
+    # catch the order of the post that user is requesting to watch , it means that we are going to underestate that the post where putted
+    for i , p in enumerate(all_posts):
+        if p.id == pid:
+            current_index = i
+            break
+    # making the prev and next posts
+    prev_post = all_posts[current_index - 1] if current_index - 1 >= 0 else None 
+    next_post = all_posts[current_index + 1] if current_index + 1 < len(all_posts) else None
+
+    # getting specific post from the database by using the get object or 404 method 
+    current_post = get_object_or_404(all_posts, pk = pid)
+    # having auto increment about our post count of views 
+    current_post.counted_view += 1
+    current_post.save() 
+    context = {'Post' : current_post, 'prev_post' : prev_post , 'next_post' : next_post}
+    return render(request , 'blog/blog-single.html', context)  
 
