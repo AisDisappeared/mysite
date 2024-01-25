b74f66a0d8157b28036750059df0cc1c2039a84d
Ali seyfi <153763977+AisDisappeared@users.noreply.github.com>
2024-01-26 00:41:47 +0330

    inclusion , filter  , simple tags
    used inclusion , filter and simple template tags in my test html template page

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -3,14 +3,28 @@ from blog.models import post
 from django.utils import timezone
 
 register = template.Library()
-
-@register.simple_tag
+# simple tag
+@register.simple_tag(name='totalposts')
 def posts_numbs():
     now = timezone.now()
     count_of_posts = post.objects.filter(status = True , published_date__lte=now).count()
     return count_of_posts 
 
+# simple tag
+@register.simple_tag(name='posts')
+def all_posts():
+    now = timezone.now()
+    posts = post.objects.all()
+    return posts
 
+# filter tag
 @register.filter
-def truncate(content,arg=20):
+def snippet(content,arg=20):
     return content[:arg]
+
+
+# inclusion tag 
+@register.inclusion_tag('popularposts.html')
+def popularposts():
+    posts = post.objects.filter(status = True).order_by('published_date')[:4]
+    return {'posts' : posts}
