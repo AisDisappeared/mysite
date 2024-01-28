82e1703eafda1e8e4c12b8c43e0540e4304d0727
Ali seyfi <153763977+AisDisappeared@users.noreply.github.com>
2024-01-28 19:52:47 +0330

    pagination
    added setting of pagination for my blog home at blog_view function view

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from unicodedata import category
 from django.shortcuts import render, get_object_or_404
 from blog.models import post 
 from django.utils import timezone 
-
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
 
 def blog_view(request,cat_name=None,author_username=None):
@@ -13,36 +13,37 @@ def blog_view(request,cat_name=None,author_username=None):
          posts = posts.filter(category__name = cat_name)
     if author_username:
         posts = posts.filter(author__username= author_username)
+
+    p = Paginator(posts,2)
+    try:
+        page_number = request.GET.get('page')
+        posts  = p.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
+
     context = {'posts' : posts}
     return render(request , 'blog/blog-home.html' , context)   
 
 
 
-
 def blog_single(request, pid):
-    # calculating the current time
     current_time = timezone.now()
-    # set query to get all of the posts from the database
     all_posts = post.objects.filter(status = 1, published_date__lte = current_time)
-    # set the default id for the post the user is requesting to watch that 
     current_index = 0
-
     # catch the order of the post that user is requesting to watch , it means that we are going to underestate that the post where putted
     for i , p in enumerate(all_posts):
         if p.id == pid:
             current_index = i
             break
-
     # making the prev and next posts
     prev_post = all_posts[current_index - 1] if current_index - 1 >= 0 else None 
     next_post = all_posts[current_index + 1] if current_index + 1 < len(all_posts) else None
-
     # getting specific post from the database by using the get object or 404 method 
     current_post = get_object_or_404(all_posts, pk = pid)
-    # having auto increment about our post count of views 
     current_post.counted_view += 1
     current_post.save() 
-    # insert information into the context and show them to the user
     context = {'Post' : current_post, 'prev_post' : prev_post , 'next_post' : next_post}
     return render(request , 'blog/blog-single.html', context)  
 
@@ -63,4 +64,15 @@ def blog_search(request):
 
     context = {'posts' : posts}
     return render(request , 'blog/blog-home.html' , context)   
+
+
+
+
+
+
+
+
+
+
+
 
