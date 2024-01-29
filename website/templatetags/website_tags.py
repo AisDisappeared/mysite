1131efe88304c371e7a08b292cf594ed91d81679
Ali seyfi <153763977+AisDisappeared@users.noreply.github.com>
2024-01-29 15:46:11 +0330

    main website latest posts
    template tag created that set a query and gives the 6 latest posts from database and show at main website page and url of title and tags fastened

--- website/templatetags/website_tags.py
+++ website/templatetags/website_tags.py
@@ -1,6 +1,12 @@
 from django import template 
+from django.utils import timezone 
+from blog.models import post 
 
 
 register = template.Library()
 
 
+@register.inclusion_tag('website/Sixlatest.html')
+def sixlatest():
+   posts = post.objects.filter(status = True).order_by('-published_date')[:6]
+   return {'posts': posts}
