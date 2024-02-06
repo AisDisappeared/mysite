4ff8191f9ae16eb0f3d89d9ee7f51f8716adf435
Ali seyfi <153763977+AisDisappeared@users.noreply.github.com>
2024-02-06 16:26:16 +0330

    check user is logged in our not
    we can use the user.is_authenticated method if django to check the user of request the we got is logged in or not and showing some views and templates to user

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,14 @@
+from calendar import c
 from django.shortcuts import render
 
 def login_view(request):
-    return render(request, 'accoutn/login.html')
+    if request.user.is_authenticated:
+        message = f'Login successful as {request.user.username}'
+        context = {'message': message}
+    else:
+        message = f'Login not successful as {request.user.username}'
+        context = {'message': message}    
+    return render(request, 'accounts/login.html', context)
 
 # def logout_view(request):
     # return  
