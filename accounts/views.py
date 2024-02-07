d0fc8f1e3955793cd23b28912f43a4154817ffba
Ali seyfi <153763977+AisDisappeared@users.noreply.github.com>
2024-02-07 14:28:30 +0330

    Django Built-in AuthenticationForm
    used

--- accounts/views.py
+++ accounts/views.py
@@ -1,23 +1,37 @@
 from calendar import c
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 import sweetify
 
+
+
 def login_view(request):
-    if request.method == 'POST':
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            sweetify.success(request , 'login successful')
-            return redirect('/')
-    message = 'login failed'
-    context = {'message': message}
-    return render(request, 'accounts/login.html', context)
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = AuthenticationForm(request=request , data=request.POST)
+            if form.is_valid():
+                username = form.cleaned_data['username']
+                password = form.cleaned_data['password']
+                user = authenticate(request, username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    sweetify.success(request , 'login successful')
+                    return redirect('/')
+        form = AuthenticationForm()
+        context = {'form': form}
+        return render(request, 'accounts/login.html', context)
+    else:
+        return redirect('/')
+
+
 
 # def logout_view(request):
     # return  
+
+
+
+
 
 def signup_view(request):
     return render(request,'accounts/signup.html')
