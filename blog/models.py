932d741e84d22a446837eb6d257984a18a0f78a2
Ali seyfi <153763977+AisDisappeared@users.noreply.github.com>
2024-02-25 15:50:46 +0330

    comments form template
    had a mistake that fixed when the users are logged in they don't need to complete the email and email comment's fields

--- blog/models.py
+++ blog/models.py
@@ -54,7 +54,7 @@ class Comment(models.Model):
     email = models.EmailField(null=True, blank=True)
     subject = models.CharField(max_length = 255)
     message = models.TextField() 
-    approved = models.BooleanField(default = False)
+    approved = models.BooleanField(default = True)
     created_date = models.DateTimeField(auto_now_add = True)
     updated_date = models.DateTimeField(auto_now = True)
 
